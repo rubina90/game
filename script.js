e2e235e546c0a76bbddf898443637e40a07afb1e
Waldemar Bira <waldek.bira@gmail.com>
2015-06-20 00:54:45 +0200

    shooting

--- script.js
+++ script.js
@@ -33,6 +33,28 @@ $(document).ready(function() {
 
     }
 
+    function shoot(){
+        var $bullet = $("<div class='bullet'></div>");
+        $('#game').append($bullet);
+
+        $('.bullet').css('top', $('#game').height() - $('#car').height());
+        $('.bullet').css('left', parseFloat($('#car').css('left')));
+        moveBullet($bullet);
+    }
+
+    function moveBullet(elem) {
+        var refreshIntervalId = setInterval(function() {
+                var old_top = (parseFloat(elem.css('top')));
+                if(old_top <= 0) {
+                    elem.remove();
+                    clearInterval(refreshIntervalId);
+                }
+                else {
+                    elem.css('top', (old_top - 3) + 'px');
+                }
+        }, 50);
+    }
+
 
     $(document).keydown(function(e) {
         switch(e.which) {
@@ -44,7 +66,10 @@ $(document).ready(function() {
             }
                 break;
 
-            case 38: // up
+            case 32:
+            {
+                shoot();
+            }// up
                 break;
 
             case 39: // right
